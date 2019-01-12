c0a578cfa7742c6de362b9b6372e7f7e2c4fd73e
Tanish Grover <tanigrover@gmail.com>
2019-01-12 16:43:55 +0530

    replaced flask api url

--- node-server/controllers/homeController.js
+++ node-server/controllers/homeController.js
@@ -18,7 +18,6 @@ module.exports = homeController = {
 
 	postImage: async (req, res, next) => {
 		const image = req.file;
-		console.log("reacher here");
 		console.log(image);
 		if (!image) {
 			return res.status(422).render('home', {
@@ -29,36 +28,18 @@ module.exports = homeController = {
 		}
 		
 		const imageUrl = image.path;
-		const url = 'http://127.0.0.1:5000/';
+		const url = 'https://agrilearning-api.herokuapp.com/';
 		const imagePath = `${__dirname}/../${imageUrl}`
 		request.post({url: url, formData: {
 		    file: fs.createReadStream(imagePath),
-		    // ...and all other form inputs
-		    imageClass: 'twitterApp' // etc.
 		}})
 		    .then(data => {
-		        console.log(data);
+		        console.log(data.response.split("-"));
 		        fileHelper.deleteFile(imagePath);
 		    }).catch(err => {
 		        console.log(err);
 		    });
-		// let data = new FormData();
-		// data.append('file', image);
-		
-		// axios.post(URL, data, {
-		//   headers: {
-		//     'accept': 'application/json',
-		//     'Accept-Language': 'en-US,en;q=0.8',
-		//     'Content-Type': `multipart/form-data; boundary=${data._boundary}`,
-		//   }
-		// })
-		//   .then((response) => response.json())
-		//   .then(resp => console.log(resp))
-		//   .catch((error) => {
-		//     console.log(error);
-		//   });
-		
-		// const imageUrl = "uploads/2019-01-11T08:00:31.859Z-insect.jpg"
+
 		console.log(imageUrl);
 		// fs.createReadStream(req.file.path).pipe(request.post('http://127.0.0.1:5000/', (err, data) => console.log(JSON.parse(data))));
 		// let pyScriptPath = path.join(__dirname, "../pyScript/app.py");
@@ -72,11 +53,6 @@ module.exports = homeController = {
   // 		pyProg.stderr.on("data", data => {
   //   		console.log(`stderr: ${data}`);
   // 		});
-		// NOW SEND THIS URL TO CNN MODEL
-		// GET THE RESULT FROM THE MODEL
-		// SEARCH FOR THE INSECT IN YOUR DATABASE
-		// SAVE THE INFORMATION IN LOCALS
-		// DELETE IMAGE FROM SERVER
 	},
 }
 
