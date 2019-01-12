00bc1ac7aabdc0d2295437728d314f73b393ae16
Utkarsh Tyagi <utkarsh4430@gmail.com>
2019-01-12 15:38:48 +0530

    added model prediction- diseases

--- Flask-ML-Api/server.py
+++ Flask-ML-Api/server.py
@@ -3,10 +3,27 @@ from flask_cors import CORS
 import logging, os
 from werkzeug import secure_filename
 import numpy as np
-from keras.models import load_model
+from keras.models import model_from_json
 import sys
 import json
 import cv2
+
+classes = {
+    0:'Apple-Apple Scab',
+    1:'Apple-Black Rot',
+    2:'Apple-Healthy',
+    3:'Corn-Common Rust',
+    4:'Corn-Healthy',
+    5:'Peach-Bacterial Spot',
+    6:'Peach-healthy',
+    7:'Potato-Early Blight',
+    8:'Potato-Late Blight',
+    9:'Potato-Healthy',
+    10:'Tomato-Bacterial Spot',
+    11:'Tomato-Tomato Mosaic Virus',
+    12:'Tomato-Healthy'
+}
+
 
 app = Flask(__name__)
 CORS(app)
@@ -19,7 +36,17 @@ UPLOAD_FOLDER = '{}/uploads/'.format(PROJECT_HOME)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 filePath = os.getcwd() + "/model.h5"
-model = load_model(filePath)
+filePath2 = os.getcwd() + "/model.json"
+json_file = open(filePath2,"r")
+loaded_model_json = json_file.read()
+json_file.close()
+model = model_from_json(loaded_model_json)
+model.load_weights(filePath)
+
+def predict(img):
+    pred = model.predict(img)
+    final = pred.argmax()
+    return classes[final]
 
 def create_new_folder(local_dir):
     newpath = local_dir
@@ -43,13 +70,13 @@ def api_root():
         saved_path = os.path.join(app.config['UPLOAD_FOLDER'], img_name)
         app.logger.info("saving {}".format(saved_path))
         img.save(saved_path)
-        a = {"hellooooo": "from the python sideeeeee"}
+        # a = {"hellooooo": "from the python sideeeeee"}
         mat = cv2.imread(saved_path)
-        mat = cv2.resize(mat, (50, 50))
+        mat = cv2.resize(mat, (100, 100))
         mat = mat / 255.0
-        mat = mat.reshape(1, 50, 50, 3)
-        predict = model.predict(mat)
-        response_data = {"response": predict.tolist()}
+        mat = mat.reshape(1, 100, 100, 3)
+        pred = predict(mat)
+        response_data = {"response": pred }
         os.remove(os.path.join(app.config['UPLOADED_FOLDER'], img.filename))
         return jsonify(response_data);
     else:
